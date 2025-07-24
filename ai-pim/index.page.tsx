186fbb5eacc25727292100fde3d3e92f4fb05b74
Oleksandr Zyzych <oleksandr.zyzych@redocly.com>
2025-07-24 12:19:47 +0000

    test

---
+++ ai-pim/index.page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const IndexPage: React.FC = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to Our Developer Portal</h1>
+      <p>This is a custom React page using only <code>@redocly/realm</code>.</p>
+    </div>
+  );
+};
+
+export default IndexPage;
